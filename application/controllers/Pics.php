752d82208c8456df960090068d718338747e13bd
Anu Slorah <anuslorah@users.noreply.github.com>
2017-07-24 09:36:11 -0700

    beginning code
    loads images, needs header, footer and better layout

---
+++ application/controllers/Pics.php
@@ -0,0 +1,45 @@
+<?php
+//application/controllers/News.php
+class Pics extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('pics_model');
+        $this->load->helper('url_helper'); //this ay ot be visible, might be a built in helper
+        $this->config->set_item('banner', 'Pics Section');
+
+    }//end __construct
+
+    public function index() 
+    {//will show categories of pictures
+        
+      /*
+        $data['title'] = 'Picture Categories';
+            
+        $this->config->set_item('title', 'Pics');
+
+        $this->load->view('pics/index', $data);  
+      */
+        
+    }//end index
+
+    public function view($tags = NULL)
+    {//will show pictures for one category only
+    //$data['news_item'] = $this->news_model->get_news($slug);
+       
+        $data['pics'] = $this->pics_model->get_pics($tags);
+        $data['title'] = 'Picture Categories';
+            
+        $this->config->set_item('title', 'Pics');
+
+        $this->load->view('pics/view', $data);    
+
+        
+
+        //$this->config->set_item('title', $data['news_item']['title']);
+
+
+            }//end view
+
+}//end Pics class
