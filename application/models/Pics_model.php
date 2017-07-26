e3cca805b640fa70b842a410f233fe11937c8567
Anu Slorah <anuslorah@users.noreply.github.com>
2017-07-26 00:40:33 -0700

    Add files via upload
    Adds table and titles under each image but the table shows same data in each row.

--- application/models/Pics_model.php
+++ application/models/Pics_model.php
@@ -1,26 +1,39 @@
 <?php
-//application/models/News_model.php
+//application/models/Pics_model.php
 class Pics_model extends CI_Model {
 
     public function __construct()
     {
-        //$this->load->database();
+        $this->load->database();
     }
-    
-    public function get_pics($tags)
+ 
+    public function get_pics($tags = FALSE)
     {
+        /*
+        //get tags from db
+        if ($tags === FALSE)
+        {
+            $query = $this->db->get('ci_tags');
+            return $query->result_array();
+        }
+        */
+        //$tags = $this->db->get_where('ci_tags', array('tag' => $tags));
+        //return $query->row_array();
+        
+        
         //move aPI key to custom_config.php
+        //$this->config->set_item('$api_key', 'api_key');
+        
         $api_key = 'f4acfc86c234949c5e729be75eaed321';
         //$tags = 'bears,polar';
 
-        $perPage = 25;
+        $perPage = 4;
         $url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
         $url.= '&api_key=' . $api_key;
         $url.= '&tags=' . $tags;
         $url.= '&per_page=' . $perPage;
         $url.= '&format=json';
         $url.= '&nojsoncallback=1';
-
         $response = json_decode(file_get_contents($url));
         $pics = $response->photos->photo;
         
